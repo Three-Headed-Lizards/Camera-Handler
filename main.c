9ec38bb442e3a9712a741c87ae8f70c6a0fc5a19
tlincke125 <thli2739@colorado.edu>
2019-10-27 11:40:07 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+/**
+ * @author      : theo (theo.j.lincke@gmail.com)
+ * @file        : main
+ * @created     : Sunday Oct 27, 2019 11:32:05 MDT
+ */
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void) {
+  CURL* curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  curl = curl_easy_init();
+
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/robotpos");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=theo&project=test");
+
+    res = curl_easy_perform(curl);
+
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform failed: %s\n", curl_easy_strerror(res));
+    }
+
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+  return 0;
+}
